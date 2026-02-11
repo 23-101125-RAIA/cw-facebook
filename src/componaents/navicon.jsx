bd8677c7e470dec1561f73c324453b9ba32e8408
23-101125-RAIA <23-101125@students.eui.edu.eg>
2026-02-11 12:35:07 +0200

    nav

---
+++ src/componaents/navicon.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './navicon.css'
+import img2  from '../asserts/Vector.png'
+const Navicon = () => {
+    return ( <>
+    <img className='navicon' src={img2} alt="" />
+    </>);
+}
+ 
+export default Navicon;
---
+++ src/pages/home.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style.css'
+import logo from "../asserts/Group 1.png"
+import Navicon from '../componaents/navicon'
+
+const Home = () => {
+    return ( <>
+    
+ <body>
+    <nav className='nav'><img className='logo' src={logo} />
+    <div className='nav-div'>
+        <Navicon />
+          <Navicon />
+            <Navicon />
+              <Navicon />
+
+    </div>
+    
+    </nav>
+
+    
+ </body>
+    
+    </> );
+}
+ 
+export default Home;
